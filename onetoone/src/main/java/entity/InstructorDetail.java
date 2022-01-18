4e5fb26af3ed59ecdbc115a8611fd321e7989aa8
pihda <62319714+pihdastudios@users.noreply.github.com>
2022-01-18 08:27:33 +0700

    move files

---
+++ onetoone/src/main/java/entity/InstructorDetail.java
@@ -0,0 +1,75 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    public InstructorDetail() {
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InstructorDetail that = (InstructorDetail) o;
+        return id == that.id && Objects.equals(youtubeChannel, that.youtubeChannel) && Objects.equals(hobby, that.hobby);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, youtubeChannel, hobby);
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetailClass{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+
+}
