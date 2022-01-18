c623cd5de14d8a1493ec600481289418347aac4f
pihda <62319714+pihdastudios@users.noreply.github.com>
2022-01-18 10:13:11 +0700

    move files

---
+++ bidirectional/src/main/java/App.java
@@ -0,0 +1,134 @@
+import entity.InstructorDetail;
+import entity.Instructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+//    public static void main(String[] args) {
+//        var jdbcUrl = "jdbc:mariadb://localhost:3306/hb-01-one-to-one-uni?useSsl=false";
+//        var user = "root";
+//        var pass = "S3cret";
+//
+//        try {
+//            System.out.println("Connecting to database: " + jdbcUrl);
+//            Class.forName ("org.mariadb.jdbc.Driver");
+//            Connection myConn =
+//                    DriverManager.getConnection(jdbcUrl, user, pass);
+//
+//            System.out.println("Connection successful!!!");
+//
+//        } catch (Exception exc) {
+//            exc.printStackTrace();
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        // create session factory
+//        SessionFactory factory = new Configuration()
+//                .configure("hibernate.cfg.xml")
+//                .addAnnotatedClass(Instructor.class)
+//                .addAnnotatedClass(InstructorDetail.class)
+//                .buildSessionFactory();
+//
+//        // create session
+//
+//        try (factory) {
+//            Session session = factory.getCurrentSession();
+//
+//            // create the objects
+//			/*
+//			Instructor tempInstructor =
+//					new Instructor("Chad", "Darby", "darby@luv2code.com");
+//
+//			InstructorDetail tempInstructorDetail =
+//					new InstructorDetail(
+//							"http://www.luv2code.com/youtube",
+//							"Luv 2 code!!!");
+//			*/
+//
+//            Instructor tempInstructor =
+//                    new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+//
+//            InstructorDetail tempInstructorDetail =
+//                    new InstructorDetail(
+//                            "http://www.youtube.com",
+//                            "Guitar");
+//
+//            // associate the objects
+//            tempInstructor.setInstructorDetail(tempInstructorDetail);
+//
+//            // start a transaction
+//            session.beginTransaction();
+//
+//            // save the instructor
+//            //
+//            // Note: this will ALSO save the details object
+//            // because of CascadeType.ALL
+//            //
+//            System.out.println("Saving instructor: " + tempInstructor);
+//            session.save(tempInstructor);
+//
+//            // commit transaction
+//            session.getTransaction().commit();
+//
+//            System.out.println("Done!");
+//
+//
+//        }
+//    }
+
+    //    public static void main(String[] args) {
+//        // create session factory
+//        SessionFactory factory = new Configuration()
+//                .configure("hibernate.cfg.xml")
+//                .addAnnotatedClass(Instructor.class)
+//                .addAnnotatedClass(InstructorDetail.class)
+//                .buildSessionFactory();
+//
+//        // create session
+//
+//        try (factory) {
+//            Session session = factory.getCurrentSession();
+//
+//
+//            /// Delete
+//            session.beginTransaction();
+//            var tempInstructor = session.get(Instructor.class, 1);
+//            if (tempInstructor != null) {
+//                session.delete(tempInstructor);
+//                session.getTransaction().commit();
+//            }
+//
+//        }
+//    }
+
+
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+
+        try (factory) {
+            Session session = factory.getCurrentSession();
+
+
+            /// Delete
+            session.beginTransaction();
+            var tempInstructorDetail = session.get(InstructorDetail.class, 3);
+            if (tempInstructorDetail != null) {
+                System.out.println(tempInstructorDetail.getInstructor());
+                tempInstructorDetail.getInstructor().setInstructorDetail(null);
+                session.delete(tempInstructorDetail);
+            }
+            session.getTransaction().commit();
+
+        }
+    }
+
+}
---
+++ bidirectional/src/main/java/entity/Instructor.java
@@ -0,0 +1,100 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    public Instructor() {
+
+    }
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Instructor intructor = (Instructor) o;
+        return id == intructor.id && Objects.equals(firstName, intructor.firstName) && Objects.equals(lastName, intructor.lastName) && Objects.equals(email, intructor.email) && Objects.equals(instructorDetail, intructor.instructorDetail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, instructorDetail);
+    }
+
+    @Override
+    public String toString() {
+        return "Intructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+}
